b8f78bf73ef7e9f1faa2017f0d008697b5aeef62
tuananhit87 <ngtuananh87@gmail.com>
2020-07-31 19:38:52 +0700

    edit webpack dev config

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,7 +3,7 @@ const common = require('./webpack.common');
 
 module.exports = merge(common, {
     mode: 'development',
-    devtool: 'cheap-module-eval-source-map',
+    devtool: 'source-map',
     devServer: {
         //contentBase: path.join(__dirname, 'dist'),
         historyApiFallback: true
