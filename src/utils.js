508f4fa4e171f9edff483c2884bdccde81997df2
tuananhit87 <ngtuananh87@gmail.com>
2020-08-01 16:28:37 +0700

    add utils.js

---
+++ src/utils.js
@@ -0,0 +1,18 @@
+import { requirePropFactory } from "@material-ui/core"
+import { favicons } from './constants/productConstants'
+
+let randomPropValueOf = (object) => {
+    let vals = Object.values(object)
+    return vals[Math.floor((Math.random() * vals.length))]
+}
+
+export const getFavicon = (favicon) => {
+    return favicons.key(favicon) != null ? favicons.key(favicon) : ''
+}
+
+export const loadFavicon = (favicon) => {
+    const link = document.createElement('link')
+    document.head.appendChild(link)
+    link.rel = 'icon'
+    link.href = getFavicon(favicon)
+} 
